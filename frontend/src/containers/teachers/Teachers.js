e63c8553d696ff6ad1574d3a81321c8c13c10d64
shrashi <shreya.upadhyaya1506@gmail.com>
2021-12-12 22:39:26 +0530

    Added Table for teachers

--- frontend/src/containers/teachers/Teachers.js
+++ frontend/src/containers/teachers/Teachers.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Table from "../../components/Table/Table";
+
+const Column=[{name:"FacultyID",title:"ID"},
+{name:"Name",title:"Name"},
+{name:"Course",title:"Course"},
+{name:"Rating",title:"Faculty Rating"}]
+const Data1 = [{FacultyID:"ECE-141",Name:"Dr Shraddha Kapoor",Course:"Digital Electronics",Rating:"5"}]
 const Teachers = () => {
-  return <div>Welcome to Teachers Page</div>;
+  return <div>Welcome to Teachers Page
+  <Table headerColumns={Column} data={Data1}></Table>
+  </div>;
 };
 export default Teachers;
